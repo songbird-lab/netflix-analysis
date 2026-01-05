3e1fe608c7abd6ce50512ce56b69a0bb78f7b1d1
songbird-lab <songbird-lab@proton.me>
2026-01-04 22:12:42 -0500

    Initial commit: add netflix.py

---
+++ netflix.py
@@ -0,0 +1,24 @@
+import pandas as pd # used for data preparation
+import numpy as np  # used for linear operations
+import textblob as TextBlob # used for sentiment analysis
+import plotly.express as px  # used for data visualization
+
+df = pd.read_csv("netflix_titles.csv")
+
+print(df.shape)
+
+print(df.head)
+
+print(df.columns)
+
+x = df.groupby(['rating']).size().reset_index(name = 'count')
+
+print(x)
+
+pieChart = px.pie(x, values = 'count', names = 'rating', title = 'Distribution of content ratings on Netflix')
+pieChart.show()
+
+df['director'] =df['director'].fillna('Director Not Specified')
+df.head()
+
+# 11:44 approximate timestamp in video at https://www.youtube.com/watch?v=1ZftU-eU4VA&t=555s
